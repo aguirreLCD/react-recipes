a5e37545105375e97bcda120b4a34e43ad744f69
Lilian Daminelli Aguirre <lilian@Lilians-MacBook-Air.local>
2022-03-24 16:56:22 +0100

    start to add veggie recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -17,11 +17,26 @@ function Popular() {
     }, []);
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=4`);
-        const data = await api.json(); 
-        console.log(data);
-        setPopular(data.recipes);
-        console.log(data.recipes);
+
+        const check = localStorage.getItem("popular");
+
+        if (check) {
+            setPopular(JSON.parse(check));
+
+        } else {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5`);
+            
+            const data = await api.json(); 
+
+            localStorage.setItem("popular", JSON.stringify(data.recipes));
+
+            console.log(data);
+            
+            setPopular(data.recipes);
+            
+            console.log(data.recipes);
+        }
+
     }
 
     return (
@@ -41,6 +56,7 @@ function Popular() {
                             <Card>
                                 <p>{recipe.title}</p>
                                 <img src={recipe.image} alt={recipe.title} />
+                                <Gradient />
                             </Card>
                         </SplideSlide>
                     );
@@ -58,11 +74,46 @@ const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
 
     img {
         border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;   
+        justify-content: center;
+        align-items: center;
     }
 `;
 
 
-export default Popular
+
+const Gradient = styled.div`
+
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+
+`;
+
+
+export default Popular;
