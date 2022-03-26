8ad6c60b24353f74c4b1a645d21601058cd44b80
Lilian Daminelli Aguirre <lilian@Lilians-MacBook-Air.local>
2022-03-26 11:16:47 +0100

    add search component and fix routing for searched page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 // import './App.css';
 import React from "react";
 
+import Search from "./components/Search";
 import Category from "./components/Category";
-
 import Pages from './pages/Pages';
 
 import { BrowserRouter } from "react-router-dom";
-import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>  
-
+      <BrowserRouter> 
         <Search />
         <Category />
         <Pages />
-      
       </BrowserRouter>
     </div>
   );
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -4,17 +4,18 @@ import Home from "./Home";
 
 import Cuisine from "./Cuisine";
 
+import Searched from "./Searched";
+
 import { Route, Routes } from "react-router-dom";
 
 function Pages() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine/:type" element={<Cuisine />} />
-      </Routes>
-
-  ); 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+    </Routes>
+  );
 }
 
 export default Pages;
